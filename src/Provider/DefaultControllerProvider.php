7d9253b0bdb685aedae323833bbe649a24d3650f
Sylvain Witmeyer <switmeyer@crakmedia.com>
2015-06-01 18:23:26 -0400

    Fix missing implemented methods

--- src/Provider/DefaultControllerProvider.php
+++ src/Provider/DefaultControllerProvider.php
@@ -9,6 +9,7 @@
 
 namespace Crak\Api\DefaultNS\Provider;
 
+use Silex\Application;
 use Silex\ServiceProviderInterface;
 
 /**
@@ -19,4 +20,26 @@ use Silex\ServiceProviderInterface;
  */
 class DefaultControllerProvider implements ServiceProviderInterface
 {
+    /**
+     * Registers services on the given app.
+     *
+     * This method should only be used to configure services and parameters.
+     * It should not get services.
+     */
+    public function register(Application $app)
+    {
+        // TODO: Implement register() method.
+    }
+
+    /**
+     * Bootstraps the application.
+     *
+     * This method is called after all services are registered
+     * and should be used for "dynamic" configuration (whenever
+     * a service must be requested).
+     */
+    public function boot(Application $app)
+    {
+        // TODO: Implement boot() method.
+    }
 }
