3a3bc0303a36f4429d5bae66f89edd6820048c01
Jean-Michael Cyr <cyrjeanmichael@gmail.com>
2016-04-07 22:22:32 -0400

    Remove useless stuff

--- envConfig.dist.php
+++ envConfig.dist.php
@@ -1,38 +1,26 @@
 <?php
 
-
-/*
-putenv("API_DB_DRIVER=pdo_mysql");
-putenv("API_DB_NAME=api.adult-categories-service");
-putenv("API_DB_USER=root");
-putenv("API_DB_PWD=");
-putenv("ENV=DEV");
-*/
-
-
-if (extension_loaded('newrelic')) {
-    newrelic_set_appname('API - Exemple');
+if (!defined('APP_NAME')) {
+    define('APP_NAME', 'API Exemple');
 }
 
-define("HATEOAS", serialize(array(
-    //'media_categories' => $app['serviceDependencyUrl']['categories'] . '/%d',
-    // 'ads_position' => $app['serviceDependencyUrl']['ads_position'] . '/%d',
-    // 'ads_type' => $app['serviceDependencyUrl']['ads_type'] . '/%d'
-)));
-
+if (extension_loaded('newrelic')) {
+    newrelic_set_appname(APP_NAME);
+}
 
 define("SERVICE_DEPENDENCY_URL", serialize(array(
-    //'channel_banner' => 'http://api.adult-channel-service/1.0/channel_banner',
-    //'ads_position' => 'http://api.adult-channel-service/1.0/ads_position',
-    //'ads_type' => 'http://api.adult-channel-service/1.0/ads_type'
+    //'table_name' => 'http://url-to-access-the-ressource/1.0/endpoint',
+)));
+
+define("HATEOAS", serialize(array(
+    //'table_name' => $app['serviceDependencyUrl']['categories'] . '/%d'
 )));
 
 
-// define constant or use environment variables (might be useful with docker)
 const API_DB_DRIVER = 'pdo_mysql';
-const API_DB_HOST = 'localhost';
-const API_DB_NAME = 'exemple-dbname';
+const API_DB_HOST = '10.1.15.121';
+const API_DB_NAME = 'exemple_dbname';
 const API_DB_USER = 'root';
 const API_DB_PWD = '';
-const API_DB_MEMORY = '';
+const API_DB_MEMORY = false;
 const ENV = 'DEV';
