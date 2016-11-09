403b0fc7b1b0698b611d1f69f18c696c029ca40f
Jean-Michael Cyr <cyrjeanmichael@gmail.com>
2016-11-08 23:21:14 -0500

    security fix

--- envConfig.dist.php
+++ envConfig.dist.php
@@ -1,17 +1,17 @@
 <?php
 
-if (!defined('APP_NAME')) {
-    define('APP_NAME', 'API Example');
-}
+define('APP_NAME', 'API Example');
 
-if (extension_loaded('newrelic')) {
+if (extension_loaded('newrelic') && function_exists('newrelic_set_appname')) {
     newrelic_set_appname(APP_NAME);
 }
 
+// TODO : Hateoas POC to be refactored.
 define("SERVICE_DEPENDENCY_URL", serialize(array(
     //'table_name' => 'http://url-to-access-the-ressource/1.0/endpoint',
 )));
 
+// TODO : Hateoas POC to be refactored.
 define("HATEOAS", serialize(array(
     //'table_name' => $app['serviceDependencyUrl']['categories'] . '/%d'
 )));
